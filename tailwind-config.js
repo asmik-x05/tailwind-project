6251235951ecb7a1f42bcb164388765fd0624e98
Asmik Lamichhane <asmiklmc.x05@gmail.com>
2025-11-27 16:39:37 +0545

    add config file

---
+++ tailwind-config.js
@@ -0,0 +1,14 @@
+tailwind.config={
+            darkMode: "class",
+            theme:{
+                extend:{
+                colors:{
+                    primary:"#3b82f6"
+                }
+            }
+            }
+           };
+         function darkTh(){
+            document.getElementById('da-li').classList.toggle('dark');
+        }
+        
